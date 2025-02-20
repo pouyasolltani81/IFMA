f13a0c2b78d004e6294fc256822d3a6a7c25761f
ALIREZAZAVI <168830744+ALIREZAZAVI@users.noreply.github.com>
2025-02-20 21:21:40 +0330

    Update cointelegraph.py

--- scrapers/cointelegraph.py
+++ scrapers/cointelegraph.py
@@ -66,7 +66,7 @@ def scrape_news_topic_7():
             "tag": "crypto_tag",
             "source": "CoinTelegraph",
             "link": article_url,
-            'summery': 'i have dieria'
+            'summary': 'i have dieria'
         }]
 
         return news
